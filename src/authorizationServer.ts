90a97e9dfc6962f796e449a0d3427bfac8b0ecdc
orange7354 <norisio876@gmail.com>
2024-04-02 22:22:30 +0900

    add auth token action

--- src/authorizationServer.ts
+++ src/authorizationServer.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import { get } from 'http';
 import path from 'path';
+import nosql from 'nosql'
 import { buildUrl, generateRandomString } from './utils/helper';
-
 
 const app = express();
 const host = 'localhost';
 const port = 9001;
+
+const nosqlClient = nosql.load('database.nosql');
 
 
 app.set('view engine', 'ejs');
@@ -15,8 +16,7 @@ app.set('views', path.join(__dirname, '../views/authorizationServer'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-const authServer :AuthServer = {
+const authServer : AuthServer = {
     authorizationEndpoint: 'http://localhost:9001/authorize',
     tokenEndpoint: 'http://localhost:9001/token'
 };
@@ -26,7 +26,7 @@ const clients : Client[] = [
         client_id: "oauth-client-1",
         client_secret: "oauth-client-secret-1",
         redirect_uris : ["http://localhost:9000/callback"]
-    },
+    },  
 ];
 
 interface CodeEntry {
@@ -55,16 +55,17 @@ app.get('/authorize',(req :Request, res : Response) => {
     } else {
         const reqid = generateRandomString(8);
         requests[reqid] = req.query;
-        res.render('approve', {client, reqid});
+        res.render('approve', {client: client, reqid: reqid });
         return;
     }
 });
 
 
 app.post('/approve', (req : Request, res : Response) => {
-    const rapid = req.body.rapid as string;
-    const query = requests[rapid];
-    delete requests[rapid];
+    const reqid = req.body.reqid as string;
+
+    const query = requests[reqid];
+    delete requests[reqid];
 
     if(!query) {
         res.render('error', {error: 'No matching authorization request'});
@@ -75,15 +76,20 @@ app.post('/approve', (req : Request, res : Response) => {
         if(query.response_type === 'code') {
             const code = generateRandomString(8);
             codes[code] = { request : query };
-            const redirect = buildUrl(query.redirect_uri, { code, state: query.state });
+
+            const redirect = buildUrl(query.redirect_uri, {
+                code : code,
+                state: query.state 
+            });
+
             res.redirect(redirect);
             return;
         } else {
-			const urlParsed = buildUrl(query.redirect_uri, {
-				error: 'unsupported_response_type'
-			});
-			res.redirect(urlParsed);
-			return;
+            const urlParsed = buildUrl(query.redirect_uri, {
+                error: 'unsupported_response_type'
+            });
+            res.redirect(urlParsed);
+            return;
         }
 
     } else {
@@ -92,9 +98,96 @@ app.post('/approve', (req : Request, res : Response) => {
         });
         res.redirect(urlParsed);
     }
+});
+
+app.post('/token', (req : Request, res : Response) => {
+    const auth = req.headers['authorization'];
+
+    let clientId;
+    let clientSecret;
+
+    if(auth){
+        const clientCredentials = decodeClientCredentials(auth);
+        clientId = clientCredentials.id;
+        clientSecret = clientCredentials.secret;
+    }
+
+    if(req.body.client_id) {
+        if(clientId) {
+            // formパラメータとヘッダーの両方でクライアントIDが指定されている場合はエラーとする(セキュリティ上の理由)
+            console.log('Client attempted to authenticate with multiple methods');
+            res.status(401).json({error: 'invalid_client'});
+            return;
+        }
+        clientId = req.body.client_id;
+        clientSecret = req.body.client_secret;
+    }
+
+    const client = getClient(clientId)
+
+    if(!client){
+        console.log('Unknown client %s', clientId);
+        res.status(401).json({error: 'invalid_client'});
+        return;
+    }
+
+    if(client.client_secret != clientSecret) {
+        console.log('Mismatched client secret, expected %s got %s', client.client_secret, clientSecret);
+        res.status(401).json({error: 'invalid_client'});
+        return;
+    }
+
+    if (req.body.grant_type == 'authorization_code') {
+        const code = codes[req.body.code];
+
+        if(code){
+            delete codes[req.body.code];
+            if(code.request.client_id == clientId) {
+                const access_token = generateRandomString(16);
+                const refresh_token = generateRandomString(16);
+
+                nosqlClient.insert({ access_token : access_token, client_id : clientId });
+                nosqlClient.insert({ refresh_token : refresh_token, client_id : clientId });
+
+                console.log('Issuing access token %s to client %s', access_token, clientId);
+
+                const token_response = {
+                    access_token: access_token,
+                    token_type: 'Bearer',
+                    refresh_token: refresh_token
+                };
+
+                res.status(200).json(token_response);
+
+                console.log('Issued tokens for code %s', req.body.code);
+
+                return;
+            } else {
+                console.log('Client mismatch, expected %s got %s', code.request.client_id, clientId);
+                res.status(400).json({error: 'invalid_grant'});
+                return;
+            }
+        } else {
+            console.log('Unknown code, %s', req.body.code);
+            res.status(400).json({error: 'invalid_grant'});
+            return;
+        }
+    } else {
+        console.log('Unknown grant type %s', req.body.grant_type);
+        res.status(400).json({error: 'unsupported_grant_type'});
+    }
+
 
 
 });
+
+const decodeClientCredentials = function(auth : string) : { id: string, secret: string } {
+    const clientCredentials = atob(auth.slice('basic '.length)).split(':');
+    const clientId = decodeURIComponent(clientCredentials[0]);
+    const clientSecret = decodeURIComponent(clientCredentials[1]);
+    return { id: clientId, secret: clientSecret };
+};
+
 
 
 app.listen(port, () => {
