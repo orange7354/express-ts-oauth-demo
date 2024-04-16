9f7524583851a4071092d08a6f4a706640cadf46
orange7354 <norisio876@gmail.com>
2024-04-16 21:15:42 +0900

    Add token verification and nosql integration to protectedResource server

--- src/protectedResource.ts
+++ src/protectedResource.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import type { Request, Response , NextFunction } from 'express';
 import cors from 'cors';
 import path from 'path';
+import nosql from 'nosql'
+
+const nosqlClient = nosql.load('database.nosql');
+
 const app = express();
 const port = 9002;
 
@@ -12,9 +16,54 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// TODO : getAccessToken 
-app.get('/resource',(req : Request, res : Response) => {
 
+interface ResourceRequest extends Request {
+    access_token? :string
+}
+
+const resource = {
+    "name": "Protected Resource",
+	"description": "This data has been protected by OAuth 2.0"
+}
+
+const getAccessToken = (req : ResourceRequest , res : Response , next : NextFunction) => {
+    let inToken = null;
+
+    const auth = req.headers['authorization'];
+
+    // headerにBearer Tokenがある場合
+    //headerは大文字小文字を区別しないため
+    if(auth && auth.toLowerCase().indexOf('bearer') === 0 ){
+        // tokenの値自体は区別する
+        inToken = auth.slice('bearer '.length);
+    } else if ( req.body && req.body.access_token ) {
+        //bodyの中を検証
+        inToken = req.body.access_token;
+    } else if (  req.query && req.query.access_token ) {
+        inToken = req.query.access_token;
+    }
+
+    nosqlClient.one().make(function(builder :any) {
+        builder.where('access_token', inToken);
+        builder.callback(function(err :any ,token :any) {
+            if (token) {
+                console.log("We found a matching token: %s", inToken);
+            } else {
+                console.log('No matching token was found.');
+            };
+            req.access_token = token;
+            next();
+            return;
+        });
+    });
+}
+
+app.get('/resource',getAccessToken,(req : ResourceRequest, res : Response) => {
+    if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
 });
 
 
